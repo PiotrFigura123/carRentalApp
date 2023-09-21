b4143aac8df1c2d65781e6435b79b7dcd8fce13b
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2023-09-21 11:23:56 +0200

    login problem

--- src/main/java/pl/piotrFigura/backendcarrental/springsecutiry/ApplicationSpringSecurityConfig.java
+++ src/main/java/pl/piotrFigura/backendcarrental/springsecutiry/ApplicationSpringSecurityConfig.java
@@ -22,45 +22,45 @@ import javax.crypto.SecretKey;
 import static pl.piotrFigura.backendcarrental.springsecutiry.ApplicationUserPermission.*;
 import static pl.piotrFigura.backendcarrental.springsecutiry.ApplicationUserRole.*;
 
-@RequiredArgsConstructor
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ApplicationSpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final PasswordEncoder passwordEncoder;
-    private final ApplicationUserService applicationUserService;
-    private final SecretKey secretKey;
-    private final JwtConfig jwtConfig;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
-                .addFilterAfter(new JwtTokenVerifier(jwtConfig, secretKey), JwtUsernameAndPasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/v1/car**").hasAuthority(GET_CAR.getPermission())
-                .antMatchers(HttpMethod.POST, "/api/v1/car**").hasAuthority(ADD_CAR.getPermission())
-                .antMatchers(HttpMethod.PUT, "/api/v1/car**").hasAnyRole(ADD_CAR.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/api/v1/car**").hasAnyRole(ADMIN.name(), BRANCH.name())
-                .anyRequest()
-                .authenticated();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(applicationUserService);
-        return provider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-}
+////@RequiredArgsConstructor
+////@Configuration
+////@EnableWebSecurity
+////@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class ApplicationSpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final PasswordEncoder passwordEncoder;
+//    private final ApplicationUserService applicationUserService;
+//    private final SecretKey secretKey;
+//    private final JwtConfig jwtConfig;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+//                .addFilterAfter(new JwtTokenVerifier(jwtConfig, secretKey), JwtUsernameAndPasswordAuthenticationFilter.class)
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/api/v1/car**").hasAuthority(GET_CAR.getPermission())
+//                .antMatchers(HttpMethod.POST, "/api/v1/car**").hasAuthority(ADD_CAR.getPermission())
+//                .antMatchers(HttpMethod.PUT, "/api/v1/car**").hasAnyRole(ADD_CAR.getPermission())
+//                .antMatchers(HttpMethod.DELETE, "/api/v1/car**").hasAnyRole(ADMIN.name(), BRANCH.name())
+//                .anyRequest()
+//                .authenticated();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(passwordEncoder);
+//        provider.setUserDetailsService(applicationUserService);
+//        return provider;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+//}
