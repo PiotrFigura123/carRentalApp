98b08f9cbaa037337a021937f1c6f624a1711445
P.Figura@il-pib.pl <p.figura@il-pib.pl>
2023-09-25 14:56:46 +0200

    clean

--- src/main/java/pl/piotrFigura/backendcarrental/securityKeycloak/SecurityConfiguration.java
+++ src/main/java/pl/piotrFigura/backendcarrental/securityKeycloak/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticatio
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -40,8 +39,6 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
     protected void configure(final HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-                .antMatchers("/addMark*")
-                .hasRole("USER")
                 .anyRequest()
                 .permitAll();
     }
